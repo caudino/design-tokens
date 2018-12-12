16aa6683721645bf308edbb2908b4084d4ec4576
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2018-12-12 14:47:47 +0100

    feat(media-queries): add extra-small and extra-large mq

--- src/attrs/media-queries.ts
+++ src/attrs/media-queries.ts
@@ -6,14 +6,16 @@ const MQ = {
   xl: 1200
 };
 
-function roundMQ(mq: number) {
+function roundMQ(mq: number): number {
   return mq - 0.02;
 }
 
 const range = {
+  extraSmall: `(max-width: ${MQ.xs})`, // Extra small devices (phones, less than 768px)
   small: `(min-width: ${MQ.sm}px) and (max-width: ${roundMQ(MQ.md)}px)`, // Small devices only (tablets, 768px and up)
   medium: `(min-width: ${MQ.md}px) and (max-width: ${roundMQ(MQ.lg)}px)`, // Medium devices only (desktops, 992px and up)
-  large: `(min-width: ${MQ.lg}px) and (max-width: ${roundMQ(MQ.xl)}px)` // Large devices only (large desktops, 1200px and up)
+  large: `(min-width: ${MQ.lg}px) and (max-width: ${roundMQ(MQ.xl)}px)`, // Large devices only (large desktops, 1200px and up)
+  extraLarge: `(min-width: ${MQ.xl})`
 };
 
 export default {
