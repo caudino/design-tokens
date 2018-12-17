f345fdc87b8eb46a0af0da9239daec4d9dcdcfbb
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2018-12-17 10:55:51 +0100

    feat(palette-schema): update ts interface

---
+++ src/attrs/colors/palette-shema.ts
@@ -0,0 +1,15 @@
+export interface PaletteSchema {
+  base: string;
+  digital: string;
+  batman: string;
+  error: string;
+  info: string;
+  success: string;
+  warning: string;
+  neutral100: string;
+  neutral200: string;
+  neutral300: string;
+  neutral400: string;
+  neutral500: string;
+  neutral600: string;
+}
