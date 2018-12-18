7b92dd357af3166cab93b48d3edc97b8d4f44dfd
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2018-12-18 17:04:06 +0100

    chore(misc): remove customScrollbar from helpers/misc

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -1,20 +1,3 @@
-import {getPalette} from './color';
-const colors = getPalette('default');
-
 export function roundMQ(mq: number): number {
   return mq - 0.02;
 }
-
-export const customScrollbar = `
-  &::-webkit-scrollbar {
-    width: 0.6em;
-    height: 6px;
-    background-color: transparent;
-    border-radius: 10px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: ${colors.neutral300};
-    border-radius: 10px;
-  }
-`;
