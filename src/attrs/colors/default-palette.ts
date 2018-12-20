aae0bb31ad901f96ad61338e4f7db83337b5f101
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2018-12-20 11:41:49 +0100

    fix(palettes): convert rgba color to hex

--- src/attrs/colors/default-palette.ts
+++ src/attrs/colors/default-palette.ts
@@ -2,23 +2,23 @@ import {PaletteSchema} from './palette-shema';
 
 export const defaultPalette: PaletteSchema = {
   // --- Base
-  base: 'rgba(255, 255, 255, 1)',
+  base: '#FFF',
 
   // --- Main
-  digital: 'rgba(3, 145, 236, 1)',
-  batman: 'rgba(0, 20, 32, 1)',
+  digital: '#0391EC',
+  batman: '#001420',
 
   // --- Services
-  error: 'rgba(245, 34, 45, 1)',
-  info: 'rgba(230, 247, 255, 1)',
-  success: 'rgba(82, 196, 26, 1)',
-  warning: 'rgba(250, 173, 20, 1)',
+  error: '#F5222D',
+  info: '#E6F7FF',
+  success: '#52C41A',
+  warning: '#FAAD14',
 
   // --- Neutrals
-  neutral100: 'rgba(247, 252, 255, 1)',
-  neutral200: 'rgba(222, 235, 244, 1)',
-  neutral300: 'rgba(195, 212, 224, 1)',
-  neutral400: 'rgba(156, 176, 191, 1)',
-  neutral500: 'rgba(91, 118, 140, 1)',
-  neutral600: 'rgba(29, 51, 68, 1)'
+  neutral100: '#F7FCFF',
+  neutral200: '#DEEBf4',
+  neutral300: '#C3D4E0',
+  neutral400: '#9CB0BF',
+  neutral500: '#5B768C',
+  neutral600: '#1D3344'
 };
--- src/attrs/colors/legacy-palette.ts
+++ src/attrs/colors/legacy-palette.ts
@@ -2,23 +2,23 @@ import {PaletteSchema} from './palette-shema';
 
 export const legacyPalette: PaletteSchema = {
   // --- Base
-  base: 'rgba(255, 255, 255, 1)',
+  base: '#FFF',
 
   // --- Main
-  digital: 'rgba(0, 114, 188, 1)',
-  batman: 'rgba(92, 105, 112, 1)',
+  digital: '#0072BC',
+  batman: '#5C6970',
 
   // --- Services
-  error: 'rgba(244, 67, 54, 1)',
-  info: 'rgba(3, 169, 244, 1)',
-  success: 'rgba(76, 175, 80, 1)',
-  warning: 'rgba(255, 152, 0, 1)',
+  error: '#F44336',
+  info: '#03A9f4',
+  success: '#4CAf50',
+  warning: '#FF9800',
 
   // --- Neutrals
-  neutral100: 'rgba(245, 247, 250, 1)',
-  neutral200: 'rgba(230, 233, 237, 1)',
-  neutral300: 'rgba(204, 209, 217, 1)',
-  neutral400: 'rgba(170, 178, 189, 1)',
-  neutral500: 'rgba(101, 109, 120, 1)',
-  neutral600: 'rgba(67, 74, 90, 1)'
+  neutral100: '#F5F7FA',
+  neutral200: '#E6E9ED',
+  neutral300: '#CCD1D9',
+  neutral400: '#AAB2BD',
+  neutral500: '#656D78',
+  neutral600: '#434A5A'
 };
