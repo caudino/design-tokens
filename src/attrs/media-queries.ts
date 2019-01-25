03803384c48d9667271283576dab4f24984888bf
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2019-01-25 16:20:53 +0100

    fix: media-queries export

--- src/attrs/media-queries.ts
+++ src/attrs/media-queries.ts
@@ -1,6 +1,6 @@
 import {roundMQ} from '../helpers/misc';
 
-export const size = {
+export const sizes = {
   xs: 320,
   sm: 576,
   md: 768,
@@ -9,16 +9,16 @@ export const size = {
 };
 
 export const range = {
-  extraSmall: `(max-width: ${size.xs})`, // Extra small devices (phones, less than 768px)
-  small: `(min-width: ${size.sm}px) and (max-width: ${roundMQ(size.md)}px)`, // Small devices only (tablets, 768px and up)
-  medium: `(min-width: ${size.md}px) and (max-width: ${roundMQ(size.lg)}px)`, // Medium devices only (desktops, 992px and up)
-  large: `(min-width: ${size.lg}px) and (max-width: ${roundMQ(size.xl)}px)`, // Large devices only (large desktops, 1200px and up)
-  extraLarge: `(min-width: ${size.xl})`
+  extraSmall: `(max-width: ${sizes.xs})`, // Extra small devices (phones, less than 768px)
+  small: `(min-width: ${sizes.sm}px) and (max-width: ${roundMQ(sizes.md)}px)`, // Small devices only (tablets, 768px and up)
+  medium: `(min-width: ${sizes.md}px) and (max-width: ${roundMQ(sizes.lg)}px)`, // Medium devices only (desktops, 992px and up)
+  large: `(min-width: ${sizes.lg}px) and (max-width: ${roundMQ(sizes.xl)}px)`, // Large devices only (large desktops, 1200px and up)
+  extraLarge: `(min-width: ${sizes.xl})`
 };
 
-export const MQ = {
-  small: `(max-width: ${size.md}px)`, // Small devices (tablets, 768px and up)
-  medium: `(max-width: ${size.lg}px)`, // Medium devices (desktops, 992px and up)
-  large: `(max-width: ${size.xl}px)`, // Large devices (large desktops, 1200px and up)
+export const breakpoints = {
+  small: `(max-width: ${sizes.md}px)`, // Small devices (tablets, 768px and up)
+  medium: `(max-width: ${sizes.lg}px)`, // Medium devices (desktops, 992px and up)
+  large: `(max-width: ${sizes.xl}px)`, // Large devices (large desktops, 1200px and up)
   range
 };
