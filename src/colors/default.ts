cf6479b6a0492a79f44eb6d51bdc7d603f9a7459
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2019-03-22 18:10:38 +0100

    update default palette value

---
+++ src/colors/default.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Contactlab UXD Team <https://ux.contactlab.com>.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const defaultPalette = {
+  'primary-color': '#0072bc',
+  'link-color': '#0072bc',
+  'processing-color': '#0072bc',
+  'highlight-color': '#f44336',
+  'error-color': '#f44336',
+  'warning-color': '#ff9800',
+  'success-color': '#4caf50',
+  'heading-color': '#656d78',
+  'normal-color': '#e6e9ed',
+  'text-color': '#434a5a',
+  'text-color-secondary': '#aab2bd',
+  'disabled-color': '#e6e9ed',
+  'border-color-base': '#ccd1d9',
+  'box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)',
+  white: '#fff',
+  black: '#000'
+};
---
+++ src/colors/illustrations.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) Contactlab UXD Team <https://ux.contactlab.com>.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const illustrationsPalette = {
+  'fluo-accent-color': '#00c9bd',
+  'fluo-base-color': '#7168ff'
+};
