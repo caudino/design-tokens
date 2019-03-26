1849a7f3a69a0f20e69986fcc40acd5d2c5188d8
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2019-03-26 12:03:04 +0100

    fix illustration palette keyword

--- lib/colors/illustration/index.d.ts
+++ lib/colors/illustration/index.d.ts
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export declare const defaultPalette: {
+export declare const illustrationsPalette: {
   colorFluoAccent: string;
   colorFluoBase: string;
 };
