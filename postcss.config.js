a32955e2bd6f4076f363595abde3127d7f73f782
Giovanni Tramutola <tramutola.giovanni@gmail.com>
2019-04-16 12:40:20 +0200

    add illustrations svg

---
+++ postcss.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const cssnano = require('cssnano');
+const postcssNested = require('postcss-nested');
+
+module.exports = {
+  plugins: [
+    cssnano({
+      preset: [
+        'advanced',
+        {
+          autoprefixer: {
+            grid: true,
+            browsers: [
+              'last 2 versions',
+              'not ie <= 11',
+              'not op_mini all',
+              'not dead',
+              'not < 0.5%'
+            ]
+          }
+        }
+      ]
+    }),
+    postcssNested
+  ]
+};
